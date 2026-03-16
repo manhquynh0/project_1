e44e7969d09ed36e4026f72b9a58140116275ace
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-03-16 21:39:38 +0700

    layouts partials pages

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// Thiet lap view
+app.set('views',path.join(__dirname,"views"))
+app.set('view engine','pug')
+//
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("client/pages/home.pug" ,
+    { pageTitle : "Trang chu"}
+  )
 })
 app.get('/tours', (req,res) => {
-   res.send("danh sachhh tour")
+   res.render("client/pages/tour-list" , {
+    pageTitle : "danh sach tour"
+})
 })
 app.get('/cart', (req,res) => {
-   console.log("vao trang cart")
+
    res.send("giooo hang")
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
