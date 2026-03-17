02971aa675a9d158eb5fcddef9719c5f22f8cac9
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-03-17 20:07:17 +0700

    add mongoose

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://bugbug:Quynh%402005@cluster0.w5nb9za.mongodb.net/tour-management');
+const Tour = mongoose.model('Tour', {
+  name: String,
+  major: String,
+  age: Number
+}, )
+
+
+
 const app = express()
 const port = 3000
 
 // Thiet lap view
-app.set('views',path.join(__dirname,"views"))
-app.set('view engine','pug')
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'pug')
 //
 
 //Thiết lâp thư mục chứa file tĩnh bên FE
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")))
 //
 
 app.get('/', (req, res) => {
-  res.render("client/pages/home.pug" ,
-    { pageTitle : "Trang chu"}
-  )
+  res.render("client/pages/home.pug", {
+    pageTitle: "Trang chu"
+  })
 })
-app.get('/tours', (req,res) => {
-   res.render("client/pages/tour-list" , {
-    pageTitle : "danh sach tour"
-})
-})
-app.get('/cart', (req,res) => {
+app.get('/tours', async (req, res) => {
+  const tourList = await Tour.find({})
+  console.log(tourList)
+  res.render("client/pages/tour-list", {
+    pageTitle: "danh sach tour",
+    tourList: tourList // truyen vao tourlist
 
-   res.send("giooo hang")
+  })
+})
+app.get('/cart', (req, res) => {
+
+  res.send("giooo hang")
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
