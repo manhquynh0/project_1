c310ec97d5b2998b076e9d7e250f4eb6b669514f
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-03-19 20:44:53 +0700

    add category-create

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,5 +1,10 @@
 module.exports.list = async(req,res) => {
     res.render("admin/pages/category-list", {
-    pageTitle : "Dach sach"
+    pageTitle : "Dach sach danh muc"
+    })
+}
+module.exports.create = async(req,res) => {
+    res.render("admin/pages/category-create", {
+    pageTitle : "Tao danh muc"
     })
 }
