0eb3a84897fc8cc14dc613428caccba7218c2a80
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-03-31 19:58:13 +0700

    validate category

--- routers/admin/category.router.js
+++ routers/admin/category.router.js
@@ -1,12 +1,12 @@
 const router = require("express").Router()
 const multer = require("multer")
 const cloudinaryHelper = require("../../helpers/cloudinary.helper")
-
+const categoryValidate = require("../../validates/admin/category.validate")
 const upload = multer({storage : cloudinaryHelper.storage})
 const categoryController = require("../../controllers/admin/category.controller")
 router.get("/list",categoryController.list)
 router.get("/create",categoryController.create)
 
 
-router.post('/create', upload.single('avatar'), categoryController.createPost)
+router.post('/create', upload.single('avatar'), categoryValidate.categoryPost, categoryController.createPost)
 module.exports = router
