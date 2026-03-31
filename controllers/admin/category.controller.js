b580acf5df2b4e92378980bac03d886c4ac87585
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-03-31 19:47:31 +0700

    add category tree

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,12 +1,20 @@
 const Category = require("../../models/category.model")
+const categoryHelper = require("../../helpers/category.helper");
 module.exports.list = async (req, res) => {
     res.render("admin/pages/category-list", {
         pageTitle: "Dach sach danh muc"
     })
 }
 module.exports.create = async (req, res) => {
+    const categoryList = await Category.find({
+        deleted: false
+    })
+    const categoryTree = categoryHelper.buildCategoryTree(categoryList)
+
+    console.log(categoryTree)
     res.render("admin/pages/category-create", {
-        pageTitle: "Tao danh muc"
+        pageTitle: "Tao danh muc",
+        categoryList : categoryTree
     })
 }
 module.exports.createPost = async (req, res) => {
---
+++ helpers/category.helper.js
@@ -0,0 +1,24 @@
+const Category = require("../models/category.model");
+
+// Lấy cây danh mục
+const buildCategoryTree = (categories, parentId = "") => {
+  const tree = [];
+
+  categories.forEach(item => {
+    if(item.parent == parentId) {
+      const children = buildCategoryTree(categories, item.id);
+
+      tree.push({
+        id: item.id,
+        name: item.name,
+        slug: item.slug,
+        children: children
+      })
+    }
+  })
+
+  return tree;
+}
+
+module.exports.buildCategoryTree = buildCategoryTree;
+// Hết Lấy cây danh mục
